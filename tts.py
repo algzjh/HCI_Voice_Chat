cd5e28b27eafb24f2feec25778392f907c17fa64
algzjh <jiehuizhou@outlook.com>
2018-11-22 16:52:22 +0800

    Update tts.py

--- tts.py
+++ tts.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 from aip import AipSpeech
 
-APP_ID = '14861362'
-API_KEY = '8tiU0rNiokuo3uyQtvRTtKr1'
-SECRET_KEY = 'vAx7MGoZGiiZQhz9VMC5L3KaSg8eBqbt'
+APP_ID = ''
+API_KEY = ''
+SECRET_KEY = ''
 
 def text2Mp3(msg, id, my_per, my_vol, my_sdp, my_pit):
     filename = './audio/audio' + str(id) + '.mp3'
